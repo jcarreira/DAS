3c4a0696e92ae8d136cda613851afe686c90e11e
Joao Carreira <joao@berkeley.edu>
2014-11-12 17:48:46 -0800

    Added functionality to Queue

--- src/queue_object/Queue.cpp
+++ src/queue_object/Queue.cpp
@@ -1,19 +1,50 @@
 #include "Queue.hpp"
+#include <Error.hpp>
 
-using DAS::QueueLayer::Queue;
-
-template<class T>
-Queue::enqueue() {
-
-}
+using DAS::Error;
 
 template<class T>
-T* Queue::tail() {
+void Queue<T>::enqueue(const T& t) {
+    if (queue_head == queue_tail - 1
+            || (queue_head == queue_data_size
+                && queue_tail == 0))
+        throw new Error("Queue is full");
+    queue_data[queue_head] = t;
 
+    queue_head++;
+    if (queue_head == queue_data_size) // overflow
+        queue_head = 0; 
 }
 
-void Queue::dequeue() {
+template<class T>
+T Queue<T>::tail() const {
+    if (is_empty())
+        throw Error("Queue is empty");
 
+    return queue_data[queue_tail];
 }
 
+template<class T>
+void Queue<T>::dequeue() {
+    queue_tail++;
+}
 
+template<class T>
+void Queue<T>::build_queue() {
+    queue_data = new T[QUEUE_SIZE];
+
+    if (!queue_data)
+        throw Error("Not enough memory");
+}
+
+template<class T>
+int Queue<T>::size() const {
+    if (queue_head >= queue_tail)
+        return queue_head - queue_tail;
+    else return queue_head + (queue_data_size - queue_tail);
+}
+
+template<class T>
+bool Queue<T>::is_empty() const {
+    return size() == 0;
+}
--- src/queue_object/Queue.hpp
+++ src/queue_object/Queue.hpp
@@ -1,23 +1,39 @@
-#ifndef _QUEUE_H_
-#define _QUEUE_H_
+/*
+ * Circular queue
+ */
 
-namespace DAS {
-    namespace QueueLayer {
 
-        template<class T>
-        class Queue {
-            public:
-                void enqueue(const T&);
-                void dequeue();
-                T& tail();
+#ifndef _DAS_QUEUE_H_
+#define _DAS_QUEUE_H_
 
-            private:
-                void build_queue();
+#include <string>
 
-                T* queue_data;
-                int queue_size;
-        };
-    } // DAS
-} // Queue
+template<class T>
+class Queue {
+    public:
+        Queue(uint64_t queue_address) : address(queue_address), queue_data_size(QUEUE_SIZE) {
+            build_queue();
+        }
 
-#endif // _QUEUE_H_
+        void enqueue(const T&);
+        void dequeue();
+        T tail() const;
+
+        bool is_empty() const;
+        int size() const;
+        
+        void build_queue();
+
+    private:
+        static const int QUEUE_SIZE = 1024;
+
+        uint64_t address;
+        T* queue_data;
+
+        int queue_data_size;
+        int queue_head; // requests added to the head
+        int queue_tail; // requests removed from the queue
+};
+
+#endif // _DAS_QUEUE_H_
+
