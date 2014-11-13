468bda15cfc8f7f44261b782b3c78eeafb60fcda
Joao Carreira <joao@berkeley.edu>
2014-11-12 17:50:29 -0800

    Added Error exception. Removed constructor from Requst.cpp

---
+++ src/Error.hpp
@@ -0,0 +1,16 @@
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#include <string>
+
+namespace DAS {
+    class Error {
+        public:
+            Error(const std::string error_msg);
+        private:
+    };
+}
+
+#endif // _ERROR_H_
+
+
